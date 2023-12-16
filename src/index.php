63b533bc6ebf53961e58f427b695bf2d637e8ac4
Aiman <84469308+Wantan3004@users.noreply.github.com>
2023-12-16 14:42:42 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
+<head>
+<title>Page Title</title>
+	  <link rel="stylesheet" href="styles.css">
+</head>
 <body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+<?php
+
+	echo "<h1>Alhamdulillah</h2>";
+	
+?>
 
 </body>
 </html>
